d18fbf26ea57ad04ba415de5df22832a572cde8e
Katie Gu <katiegu@Katies-MacBook-Pro-5.local>
2020-06-22 18:59:55 -0700

    Testing again.

--- csmconnect/csmconnect/settings.py
+++ csmconnect/csmconnect/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','61_9p39+kjh)0m0t6%inglpq^ntd%nhw_8cd85^j-0u8pgjf=$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '')
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['csmconnect.herokuapp.com']
+# ALLOWED_HOSTS = ['csmconnect.herokuapp.com']
 
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
 
