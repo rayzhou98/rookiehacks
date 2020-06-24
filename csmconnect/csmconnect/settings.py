6f22d2b3164e8f305e1adcd57bd15dbd36469002
Katie Gu <katiegu@Katies-MacBook-Pro-5.local>
2020-06-24 15:21:31 -0700

    Added back CDN for static files.

--- csmconnect/csmconnect/settings.py
+++ csmconnect/csmconnect/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'csmconnect.urls'
