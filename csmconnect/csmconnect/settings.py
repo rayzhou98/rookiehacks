52545a5838907f4b2a39e11a70ac01f97c406d2a
Katie Gu <katiegu@Katies-MacBook-Pro-5.local>
2020-06-24 14:50:17 -0700

    Added additional packages for S3.

--- csmconnect/csmconnect/settings.py
+++ csmconnect/csmconnect/settings.py
@@ -136,6 +136,8 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 AWS_STORAGE_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 
+AWS_S3_REGION_NAME = 'us-east-2'
+
 DATE_INPUT_FORMATS = ['%a, %b %d, %Y']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
