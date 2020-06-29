cd7c90fcba91c4a045e953e2491e3af1911568fd
Katie Gu <katiegu@Katies-MacBook-Pro-5.local>
2020-06-29 16:18:34 -0700

    Debugging server error.

--- csmconnect/csmconnect/settings.py
+++ csmconnect/csmconnect/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','61_9p39+kjh)0m0t6%inglpq^ntd%nhw_8cd85^j-0u8pgjf=$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
 
 ALLOWED_HOSTS = ['csmconnect.herokuapp.com']
 
